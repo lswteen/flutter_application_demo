9e15f5e2ee2d73dcea739582ad260e837c82e1e9
leesoonwoo <lswteen@gmail.com>
2024-08-14 09:43:21 +0900

    [main]
    splash 애니메이션 효과추가

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:namer_app/ui/food_list_page.dart';  // 패키지 경로에 맞게 수정하세요.
 
+import 'food_list_page.dart';  // 패키지 경로에 맞게 수정하세요.
 import '../widgets/splash_logo.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,22 +11,53 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
   @override
   void initState() {
     super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+
+    _controller.forward();
+
     Timer(const Duration(seconds: 3), () {
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (_) => const FoodListPage()),
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => const FoodListPage(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        ),
       );
     });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: const SplashLogo(),
+      body: FadeTransition(
+        opacity: _animation,
+        child: const SplashLogo(),
+      ),
     );
   }
 }
